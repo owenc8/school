a42be1126bc51447fc99603cb2881ac9c27d1ecb
mv-nguyen <hnguyen324@my.bcit.ca>
2026-03-07 00:09:55 -0800

    Added in the non-synced pattern

---
+++ patterns/course-study-guide-list.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Course Study Guide List
+ * Slug: fwd-school/course-study-guide-list
+ * Categories: Columns, Text
+ */
+?>
+<!-- wp:media-text {"mediaId":43,"mediaLink":"http://localhost:8888/school/team-working/","mediaType":"image","mediaWidth":39,"imageFill":true,"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-media-text is-stacked-on-mobile is-image-fill-element" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);grid-template-columns:39% auto"><figure class="wp-block-media-text__media"><img src="http://localhost:8888/school/wp-content/uploads/2026/02/team-working-1024x683.jpg" alt="team working together on laptops" class="wp-image-43 size-full" style="object-position:50% 50%"/></figure><div class="wp-block-media-text__content"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"large","fontFamily":"noto-sans"} -->
+<p class="has-noto-sans-font-family has-large-font-size"><strong>Course Number- Study Guide List</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item {"fontSize":"medium"} -->
+<li class="has-medium-font-size">Course </li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div></div>
+<!-- /wp:media-text -->
